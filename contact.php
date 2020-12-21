5f0698d6b88a03094ecc1c295a031b77626d3c7f
Beenu <beenu.mca@gmail.com>
2020-12-21 13:16:09 +0530

    Added more htmls

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Home Page</title>
+    <title>Contact Page</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 </head>
