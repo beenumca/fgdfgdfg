736600e86252dd1fed75f38e8822f1fa5009ad41
Beenu <beenu.mca@gmail.com>
2020-12-21 12:53:10 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="container" style="margin-top: 30px;">
+        <div class="row">
+            <div class="col-md-4" style="text-align: center;">Section One</div>
+            <div class="col-md-4" style="text-align: center;">Section Two</div>
+            <div class="col-md-4" style="text-align: center;">Section Three</div>
+        </div>
+    </div>
+
+</body>
+</html>
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
